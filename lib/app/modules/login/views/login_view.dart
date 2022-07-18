66d2a9e4f9a7e1593a2b00f255395700d578b52b
Irfan Heriana <irfanheriana17@gmail.com>
2022-07-18 21:05:15 +0700

    sign in google

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/data/controller/auth_controller.dart';
 import 'package:task_management_app/app/routes/app_pages.dart';
 
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
+  final authC = Get.find<AuthController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -94,7 +96,7 @@ class LoginView extends GetView<LoginController> {
 
             Image.asset('assets/images/login.png', height: Get.height*0.5,),
             FloatingActionButton.extended(
-            onPressed: () => Get.toNamed(Routes.HOME), 
+            onPressed: () => authC.signInWithGoogle(), 
             label: const Text('Sign In With Google'), 
             icon: const Icon(Ionicons.logo_google, 
             color: Colors.white,),) 
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,40 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'package:task_management_app/app/data/controller/auth_controller.dart';
+import 'firebase_options.dart';
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/firebase_options.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
    WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  Get.put(AuthController(), permanent: true);
   runApp(
-    GetMaterialApp(
+    StreamBuilder<User?>(
+      stream: FirebaseAuth.instance
+      .authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+        }
+      return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Application",
-      initialRoute: AppPages.INITIAL,
+      initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
       getPages: AppPages.routes,
-    ),
-  );
+      );
+    },
+  ));
 }
 
-class Firebase {
-  static initializeApp() {}
-}
+// class Firebase {
+//   static initializeApp({required FirebaseOptions options}) {}
+// }
+
